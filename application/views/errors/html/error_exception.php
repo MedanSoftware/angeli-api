8f74d8d1e560021bc30968f1d27c262afe2de11d
Agung Dirgantara <agungmasda29@gmail.com>
2020-05-31 10:58:32 +0700

    Change error views to JSON response

--- application/views/errors/html/error_exception.php
+++ application/views/errors/html/error_exception.php
@@ -1,32 +1,25 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+header('content-type:application/json');
 
-<div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
+$debug_backtrace = array();
 
-<h4>An uncaught Exception was encountered</h4>
+foreach ($exception->getTrace() as $error) {
+	if (isset($error['file']) && strpos($error['file'], realpath(BASEPATH)) !== 0) {
+		array_push($debug_backtrace, array(
+			'file' => $error['file'],
+			'line' => $error['line'],
+			'function' => $error['function']
+		));
+	}
+}
 
-<p>Type: <?php echo get_class($exception); ?></p>
-<p>Message: <?php echo $message; ?></p>
-<p>Filename: <?php echo $exception->getFile(); ?></p>
-<p>Line Number: <?php echo $exception->getLine(); ?></p>
-
-<?php if (defined('SHOW_DEBUG_BACKTRACE') && SHOW_DEBUG_BACKTRACE === TRUE): ?>
-
-	<p>Backtrace:</p>
-	<?php foreach ($exception->getTrace() as $error): ?>
-
-		<?php if (isset($error['file']) && strpos($error['file'], realpath(BASEPATH)) !== 0): ?>
-
-			<p style="margin-left:10px">
-			File: <?php echo $error['file']; ?><br />
-			Line: <?php echo $error['line']; ?><br />
-			Function: <?php echo $error['function']; ?>
-			</p>
-		<?php endif ?>
-
-	<?php endforeach ?>
-
-<?php endif ?>
-
-</div>
+exit(json_encode(array(
+	'status' => 'error',
+	'message' => $exception->getMessage(),
+	'data' => array(
+		'type' => get_class($exception),
+		'filename' => $exception->getFile(),
+		'line' => $exception->getLine(),
+		'debug_backtrace' => $debug_backtrace
+	)
+)));
--- application/views/errors/html/error_php.php
+++ application/views/errors/html/error_php.php
@@ -1,33 +1,25 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+header('content-type:application/json');
 
-<div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
+$debug_backtrace = array();
 
-<h4>A PHP Error was encountered</h4>
+foreach (debug_backtrace() as $error) {
+	if (isset($error['file']) && strpos($error['file'], realpath(BASEPATH)) !== 0) {
+		array_push($debug_backtrace, array(
+			'file' => $error['file'],
+			'line' => $error['line'],
+			'function' => $error['function']
+		));
+	}
+}
 
-<p>Severity: <?php echo $severity; ?></p>
-<p>Message:  <?php echo $message; ?></p>
-<p>Filename: <?php echo $filepath; ?></p>
-<p>Line Number: <?php echo $line; ?></p>
-
-<?php if (defined('SHOW_DEBUG_BACKTRACE') && SHOW_DEBUG_BACKTRACE === TRUE): ?>
-
-	<p>Backtrace:</p>
-	<?php foreach (debug_backtrace() as $error): ?>
-
-		<?php if (isset($error['file']) && strpos($error['file'], realpath(BASEPATH)) !== 0): ?>
-
-			<p style="margin-left:10px">
-			File: <?php echo $error['file'] ?><br />
-			Line: <?php echo $error['line'] ?><br />
-			Function: <?php echo $error['function'] ?>
-			</p>
-
-		<?php endif ?>
-
-	<?php endforeach ?>
-
-<?php endif ?>
-
-</div>
+exit(json_encode(array(
+	'status' => 'error',
+	'message' => 'A PHP Error was encountered'.' | '.$message,
+	'data' => array(
+		'severity' => isset($this->levels[$severity]) ? $this->levels[$severity] : $severity,
+		'filepath' => $filepath,
+		'line' => $line,
+		'debug_backtrace' => $debug_backtrace
+	)
+)));
